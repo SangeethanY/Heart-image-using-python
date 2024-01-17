7d324b51e6cf7c53163986a764c8e92b77f34742
SangeethanY <86181038+SangeethanY@users.noreply.github.com>
2024-01-17 16:57:52 +0530

    Adding heart image code

--- demo.py
+++ demo.py
@@ -1,1 +1,13 @@
 print("helo world")
+form turtle import *
+bgcolor("yellow")
+  color("red")
+  begin_fill("")
+  pensize(3)
+  left(50)
+  forward(133)
+  circle(50,200)
+  right(140)
+  circle(50,200)
+  forward(133)
+  end_fill()
